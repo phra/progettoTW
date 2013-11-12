20fbfb07984f8757b1e080a5c1a04b9247f13b90
phra <phra@debian>
2013-11-12 07:26:15 +0100

    testing db connection

---
+++ cgi-bin/descrittore/descrittore.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  vicinoa.py
+#
+#  Copyright 2011 soncio <soncio@SONCIO-EEEPC>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+import sys
+sys.path.append('/var/www/progettoTW/cgi-bin/descrittore/')
+from mod_python import apache as A, util
+from encoding import smart_str
+from operator import itemgetter as IG
+import json, urllib2, math, csv, codecs
+
+RT = 6372795.477598
+
+def toplain(listout):
+	s = '\n'.join('RESULT|||V') + '\n'
+	for tupla in listout:
+		s += 'ID: ' + smart_str(tupla[0]) + ', LAT: ' + smart_str(tupla[1]) + ', LONG: ' + smart_str(tupla[2]) + ', DISTANZA: ' + smart_str(tupla[11]) + '.\n'
+	return s
+def tojson(listout):
+	dictoutjson = {}
+	dictout = {}
+	meta = [('creator', 'Working Group LTW 2011/2012'), ('created', '20/12/2011'), ('version', '1.1'), ('source', 'http://vitali.web.cs.unibo.it/TechWeb12/Formati'),('valid', '31/12/2011')]
+	metadata = dict(meta)
+	for t in listout:
+		dictout[t[0]] = {'lat': t[1], 'long': t[2], 'category': smart_str(t[3]), 'name': t[4], 'opening': t[5], 'closing': t[6], 'address': t[7], 'tel': t[8], 'subcategory': t[9], 'note': t[10], 'distance': t[11]}
+	dictoutjson['metadata'] = metadata
+	dictoutjson['locations'] = dictout
+	return json.dumps(dictoutjson)
+
+def toxml(listout):
+	s = '''<?xml version="1.0" encoding="UTF-8"?>
+<locations>
+	<metadata>
+		<creator>Working Group LTW 2011/2012</creator>
+		<created>12/20/2011</created>
+		<version>1.2</version>
+		<source>http://vitali.web.cs.unibo.it/TechWeb12/Formati</source>
+		<valid>02/21/2011</valid>
+		<note>Le date sono in formato americano: MM/GG/AAAA</note>
+	</metadata>
+'''
+	for t in listout:
+		s += '''<location id="%s" lat="%s" long="%s">
+		<category> %s </category>
+		<subcategory> %s </subcategory>
+		<name> %s </name>
+		<address> %s </address>
+		<tel> %s </tel>
+		<opening> %s </opening>
+		<closing> %s </closing>
+		</location>''' % (smart_str(t[0]),smart_str(t[1]),smart_str(t[2]),smart_str(t[3]),smart_str(t[9]),smart_str(t[4]),smart_str(t[7]),smart_str(t[8]),smart_str(t[5]),smart_str(t[6]))
+	s += '</locations>'
+	return s
+
+def tocsv(listout):
+	s = '"id","category","name","address","lat","long","subcategory","note","opening","closing"'
+	for t in listout:
+		s += '"%s","%s","%s","%s","%s","%s","%s","%s","%s,"%s","%s","%s","%s","%s","%s","Working Group LTW 2011/2012","2011-12-20","2012-10-01","1.1","Unknown"\r\n' % (t[0],t[3],t[4],t[7],t[1],t[2],t[9],t[10],t[5],t[6])
+	return s
+
+
+def index(req):
+    listout = []
+    if not (('application/json' in req.headers_in['Accept']) or ('application/xml' in req.headers_in['Accept']) or ('text/csv' in req.headers_in['Accept'])):
+        req.content_type = 'text/plain; charset="UTF-8"'
+        req.headers_out.add("Access-Control-Allow-Origin","*")
+        req.status = A.HTTP_NOT_ACCEPTABLE
+        req.write('406: NOT ACCEPTABLE\r\n')
+        req.write('intersezione tra content-type vuota.\r\n')
+        raise A.SERVER_RETURN, A.DONE
+
+    content_type = req.headers_in['Accept']
+    req.content_type = 'text/plain; charset="UTF-8"'
+    req.headers_out.add("Access-Control-Allow-Origin","*")
+    if req.method != 'GET':
+        req.status = A.HTTP_METHOD_NOT_ALLOWED
+        req.write('405: METHOD NOT ALLOWED\r\n')
+        req.write('metodo %s non permesso.\r\n' % req.method)
+        raise A.SERVER_RETURN, A.DONE
+
+    try:
+        parms = util.FieldStorage(req)
+        AGGRs = smart_str(parms.getfirst('aggr')).split('/')
+        latA = float(parms.getfirst('lat'))
+        lonA = float(parms.getfirst('long'))
+        MAX = smart_str(parms.getfirst('max')).lower()
+        DISTANZA = smart_str(parms.getfirst('distance')).lower()
+    except:
+        req.status = A.HTTP_NOT_ACCEPTABLE
+        req.write('406: NOT ACCEPTABLE\r\n')
+        req.write('parametri errati.')
+        req.write('i parametri sono: lat: %s, long: %s[, max: %s, distance: %s]\r\nex: http://fattanza.no-ip.org/progettoTW/vicinoa/ltw1130-farmacie/params/44.500456/11.277643/10/5000\r\n' % (smart_str(parms.getfirst('lat')), smart_str(parms.getfirst('long')), smart_str(parms.getfirst('max')), smart_str(parms.getfirst('distance'))))
+        raise A.SERVER_RETURN, A.DONE
+    
+
+#	radlatA = latA * (math.pi / 180.0)
+#	radlonA = lonA * (math.pi / 180.0)    
+#	radlatB = latB * (math.pi / 180.0)
+#	radlonB = lonB * (math.pi / 180.0)
+#	a = RT * math.acos(math.sin(radlatA) * math.sin(radlatB) + math.cos(radlatA) * math.cos(radlatB) * math.cos(radlonA - radlonB))
+#	listout.append((id, lat, long, category, name, opening, closing, address, tel, '', '', a))
+
+#	for aggr in AGGRs:
+		
+        
+        
+	conn = psycopg2.connect("dbname=trovatutto user=admin")
+	cur = conn.cursor()
+	cur.execute("SELECT %s from trovatutto;", ("*"))
+	cur.fetchone()
+	conn.commit()
+	cur.close()
+	conn.close()
+	
+	
+    if 'application/json' in content_type:
+        req.content_type = 'application/json; charset=utf-8'
+        req.write(tojson(listout))
+    elif 'application/xml' in content_type:
+        req.content_type = 'application/xml; charset=utf-8'
+        req.write(toxml(listout))
+    elif 'text/csv' in content_type:
+        req.content_type = 'text/cvs; charset=utf-8'
+        req.write(tocsv(listout))
+    elif 'text/turtle' in content_type:
+        req.content_type = 'text/turtle; charset=utf-8'
+        req.write(tottl(listout))
+    else:
+        req.content_type = 'text/plain; charset=utf-8'
+        req.write(toplain(listout))
+
+    req.status = A.OK
+    raise A.SERVER_RETURN, A.DONE
+
